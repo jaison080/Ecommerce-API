345bdbe62c16d0ed6884316ec356f8a795837a3c
jaison080 <jaisondennis080@gmail.com>
2022-07-17 14:21:58 +0530

    Inital commit

---
+++ routes/order.js
@@ -0,0 +1,90 @@
+const Order = require("../models/Order");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(201).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findOneAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const order = await Order.find({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL ORDERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+//GET MONTHLY INCOME
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const Date = new Date();
+  const lastMonth = new Date(Date.setMonth(Date.getMonth() - 1));
+  const previousMonth = new Date(Date.setMonth(lastMonth.getMonth() - 1));
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      { group: { _id: "$month", total: { $sum: "$sales" } } },
+    ]);
+    res.status(200).json(income);
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
